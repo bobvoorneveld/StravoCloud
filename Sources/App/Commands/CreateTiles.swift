d776042766b07a4c419118fe6c791e321ea9b600
Bob Voorneveld <info@bobvoorneveld.nl>
2023-01-04 12:42:31 +0100

    feature collection using correct tiles features

--- Sources/App/Commands/CreateTiles.swift
+++ Sources/App/Commands/CreateTiles.swift
@@ -6,6 +6,7 @@
 //
 
 import Vapor
+import FluentSQL
 
 struct CreateTilesCommand: Command {
     struct Signature: CommandSignature { }
@@ -19,11 +20,21 @@ struct CreateTilesCommand: Command {
             .makePromise(of: Void.self)
         
         promise.completeWithTask {
+            context.application.logger.info("Creating tiles for every activity.")
             let activities = try await StravaActivity.query(on: context.application.db).with(\.$user).all()
             for (index, activity) in activities.enumerated() {
                 context.application.logger.info("\(index + 1) of \(activities.count)")
                 try await activity.getTiles(on: context.application.db, forced: true)
             }
+                        
+            context.application.logger.info("Creating geojson for tiles of users.")
+            let users = try await User.query(on: context.application.db).all()
+            for (index, user) in users.enumerated() {
+                context.application.logger.info("user \(index + 1) of \(users.count)")
+                
+                try await user.updateGeoJSONTiles(on: context.application.db)
+            }
+            
         }
         try promise.futureResult.wait()
     }
--- Sources/App/Models/Extensions/User+GeoJSON.swift
+++ Sources/App/Models/Extensions/User+GeoJSON.swift
@@ -48,33 +48,18 @@ extension User {
         let activityFeatures = acts.map { $0.summaryFeature }
         
         req.logger.info("loading tiles")
-        var tileQuery = ActivityTile.query(on: req.db)
-            .join(StravaActivity.self, on: \ActivityTile.$activity.$id == \StravaActivity.$id)
-            .filter(StravaActivity.self, \.$user.$id == id!)
         
-        if let filter {
-            // tiles are 256 px, a high def screen is 3456 pixels wide (lets do 1.5x) -> 28 tiles
-            let tileWidth = (360 / Double(pow(2, filter.zoom)))
-            let delta = ((3456 * 1.5) / 256) * tileWidth / 2
-            let upperLeft = tranformCoordinate(filter.lng - delta, filter.lat - delta, withZoom: 14)
-            let downRight = tranformCoordinate(filter.lng + delta, filter.lat + delta, withZoom: 14)
-            
-            tileQuery = tileQuery
-                .filter(\.$x >= upperLeft.x)
-                .filter(\.$x <= downRight.x)
-                .filter(\.$y <= upperLeft.y)
-                .filter(\.$y >= downRight.y)
+        var tileFeatures = [Feature]()
+        if let data = geoJSONTiles?.data(using: .utf8) {
+            let decoder = JSONDecoder()
+            do {
+                let geometry = try decoder.decode(Geometry.self, from: data)
+                print(geometry)
+                tileFeatures.append(Feature(geometry: geometry))
+            } catch {
+                print(error)
+            }
         }
-        
-        let tiles = try await tileQuery
-            .field(\.$x)
-            .field(\.$y)
-            .field(\.$z)
-            .unique()
-            .all()
-        
-        req.logger.info("getting tile features")
-        let tileFeatues = tiles.map { $0.feature }
         
         req.logger.info("loading county features")
         var counties = Set<County>()
@@ -86,7 +71,36 @@ extension User {
         let countyFeatures = counties.map { $0.feature }
         
         req.logger.info("returning collection")
-        return FeatureCollection(features: countyFeatures + tileFeatues + activityFeatures)
+        return FeatureCollection(features: countyFeatures + tileFeatures + activityFeatures)
+    }
+    
+    func updateGeoJSONTiles(on db: Database) async throws {
+        // Creating geojson for every user
+        guard let sql = db as? SQLDatabase, let id else {
+            return
+        }
+        
+        _ = try await sql.raw("""
+            WITH
+                tiles AS (
+                    SELECT DISTINCT
+                        ST_AsGeoJSON(ST_Simplify(ST_Union(geom), 0.001)) as geojson
+                    FROM
+                        "activity_tiles"
+                    WHERE
+                        user_id = '\(raw: id.uuidString)'
+                )
+
+            UPDATE
+                users
+            SET
+                geojson_tiles = t.geojson
+
+            FROM
+                tiles as t
+            WHERE
+                id = '\(raw: id.uuidString)'
+            """).all()
     }
 }
 
